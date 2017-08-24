a5a7ef4bff6ff26b6f051068a363fc8e4ecf9236
NeilZhong <prayerz@163.com>
2017-08-24 22:10:00 +0800

    增加restful api，未实现功能。

---
+++ app/controllers/home.js
@@ -0,0 +1,59 @@
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  Article = mongoose.model('Article');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Generator-Express MVC',
+      articles: articles
+    });
+  });
+});
+
+//增加RESTful api
+router.get('/user', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'get',
+      articles: articles
+    });
+  });
+});
+
+router.post('/user', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'post',
+      articles: articles
+    });
+  });
+});
+
+router.delete('/user', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'delete',
+      articles: articles
+    });
+  });
+});
+
+router.put('/user', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'put',
+      articles: articles
+    });
+  });
+});
